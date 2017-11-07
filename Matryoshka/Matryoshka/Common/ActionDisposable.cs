a4030599e1a08cab21f010bb515e5cc8144be9ef
opiumtm <opium_tm@live.ru>
2017-11-07 04:36:31 +0300

    ...

---
+++ Matryoshka/Matryoshka/Common/ActionDisposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Matryoshka
+{
+    /// <summary>
+    /// Action wrapped as <see cref="IDisposable"/>.
+    /// </summary>
+    public sealed class ActionDisposable : IDisposable
+    {
+        private int _isDisposed;
+
+        private readonly Action _onDispose;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="onDispose">Action to wrap.</param>
+        public ActionDisposable(Action onDispose)
+        {
+            _onDispose = onDispose;
+        }
+
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+        /// <filterpriority>2</filterpriority>
+        public void Dispose()
+        {
+            if (Interlocked.Exchange(ref _isDisposed, 1) == 0)
+            {
+                _onDispose?.Invoke();
+            }
+        }
+    }
+}
---
+++ Matryoshka/Matryoshka/Common/CompositeDisposable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Matryoshka
+{
+    /// <summary>
+    /// Composite <see cref="IDisposable"/>.
+    /// </summary>
+    public sealed class CompositeDisposable : IDisposable
+    {
+        private ConcurrentStack<IDisposable> _disposables = new ConcurrentStack<IDisposable>();
+
+        /// <summary>
+        /// Add disposable to composite disposable.
+        /// </summary>
+        /// <param name="d">Disposable.</param>
+        public void Add(IDisposable d)
+        {
+            ConcurrentStack<IDisposable> s;
+            if ((s = Interlocked.CompareExchange(ref _disposables, null, null)) != null)
+            {
+                s.Push(d);
+            }
+            else
+            {
+                throw new ObjectDisposedException("CompositeDisposable");
+            }
+        }
+
+        /// <summary>
+        /// Add range of disposables to composite disposable.
+        /// </summary>
+        /// <param name="d">Disposables.</param>
+        public void AddRange(params IDisposable[] d)
+        {
+            if (d == null)
+            {
+                return;
+            }
+            ConcurrentStack<IDisposable> s;
+            if ((s = Interlocked.CompareExchange(ref _disposables, null, null)) != null)
+            {
+                s.PushRange(d);
+            }
+            else
+            {
+                throw new ObjectDisposedException("CompositeDisposable");
+            }
+        }
+
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+        /// <filterpriority>2</filterpriority>
+        public void Dispose()
+        {
+            ConcurrentStack<IDisposable> s;
+            if ((s = Interlocked.Exchange(ref _disposables, null)) != null)
+            {
+                List<Exception> errors = null;
+                while (s.TryPop(out var c))
+                {
+                    try
+                    {
+                        c?.Dispose();
+                    }
+                    catch (Exception e)
+                    {
+                        if (errors == null)
+                        {
+                            errors = new List<Exception>();
+                        }
+                        errors.Add(e);
+                    }
+                }
+                if (errors != null)
+                {
+                    throw new AggregateException(errors);
+                }
+            }
+        }
+    }
+}
